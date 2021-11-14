d54294b6d9d3cacef934b72c902d3e324b28eb3f
Vasily <Vacho.87@gmail.com>
2021-11-14 11:58:22 +0300

    Complete task

--- TollCalculator/TollCalculator.cs
+++ TollCalculator/TollCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using CommercialRegistration;
 using ConsumerVehicleRegistration;
 using LiveryRegistration;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TollCalc
 {
@@ -8,40 +9,41 @@ namespace TollCalc
     /// Class TollCalculator.
     /// </summary>
     public class TollCalculator
-    {
+    {        
         /// <summary>
         /// Calculates the toll.
         /// </summary>
         /// <param name="vehicle">The vehicle.</param>
         /// <returns>System.Decimal.</returns>
-        public static decimal CalculateToll(object vehicle) =>
-                vehicle switch
-                {
-                    Car c => c.Passengers switch
-                    {
-                        0 => 2.00m + 0.50m,
-                        1 => 2.00m,
-                        2 => 2.00m - 0.50m,
-                        _ => 2.00m - 1.00m,
-                    },
+        public static decimal CalculateToll(object vehicle)
+        {
+            if (vehicle is null)
+            {
+                throw new ArgumentNullException(nameof(vehicle));
+            }
+           
+            if (vehicle is Car car)
+            {
+                return car.CalcToll();
+            }
 
-                    Taxi t => t.Fares switch
-                    {
-                        0 => 3.50m + 0.50m,
-                        1 => 3.50m,
-                        2 => 3.50m - 0.50m,
-                        _ => 3.50m - 1.00m,
-                    },
+            if (vehicle is Taxi taxi)
+            {
+                return taxi.CalcToll();
+            }
 
-                    Bus b when ((double)b.Riders / (double)b.Capacity) < 0.50 => 5.00m + 2.00m,
-                    Bus b when ((double)b.Riders / (double)b.Capacity) > 0.90 => 5.00m - 1.00m,
-                    Bus => 5.00m,
-                    DeliveryTruck t when (t.GrossWeightClass > 5000) => 10.00m + 5.00m,
-                    DeliveryTruck t when (t.GrossWeightClass < 3000) => 10.00m - 2.00m,
-                    DeliveryTruck => 10.00m,
-                    { } => throw new ArgumentException("Unknown type of vehicle.", nameof(vehicle)),
-                    null => throw new ArgumentNullException(nameof(vehicle))
-                };
+            if (vehicle is Bus bus)
+            {
+                return bus.CalcToll();
+            }
+
+            if (vehicle is DeliveryTruck truck)
+            {
+                return truck.CalcToll();
+            }
+
+            throw new ArgumentException("Unknown type of vehicle.", nameof(vehicle));
+        }
 
         /// <summary>
         /// Gets the time peak premium.
